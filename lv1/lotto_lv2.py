493712cdd36046a99c1ac5c386cd7dd0c68985e3
devggu <pshn123@gmail.com>
2024-01-03 17:28:12 +0900

    01-03

--- lv1/lotto_lv2.py
+++ lv1/lotto_lv2.py
@@ -4,13 +4,13 @@ result = [3, 5]
 
 WIN_DICT = {0: 6, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2, 6: 1}
 
+
 def solution(lottos, win_nums):
     zeros = lottos.count(0)
     match_cnt = 0
     for i in lottos:
         if i in win_nums:
             match_cnt += 1
-    print(match_cnt + zeros, match_cnt)
     answer = [WIN_DICT[match_cnt + zeros], WIN_DICT[match_cnt]]
     return answer
 
