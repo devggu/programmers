2a3725d443bdae399b752916eae87e6726fa0665
devggu <pshn123@gmail.com>
2024-01-19 09:47:28 +0900

    card_stack

--- cpp/lv1/card_stack.cpp
+++ cpp/lv1/card_stack.cpp
@@ -6,15 +6,36 @@ using namespace std;
 vector<string> cards1 = {"i", "drink", "water"};
 vector<string> cards2 = {"want", "to"};
 vector<string> goal = {"i", "want", "to", "drink", "water"};
-//result =  yes
-
-
+// result =  yes
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
-    string answer = "";
-    return answer;
+    vector<string> answer = {};
+    vector<string> goal_temp = goal;
+    string result = "NO";
+    do {
+        if (cards1.front() == goal_temp.front()) {
+            answer.push_back(cards1.front());
+            cards1.erase(cards1.begin());
+            goal_temp.erase(goal_temp.begin());
+        } else if (cards2.front() == goal_temp.front()) {
+            answer.push_back(cards2.front());
+            cards2.erase(cards2.begin());
+            goal_temp.erase(goal_temp.begin());
+        } else {
+            break;
+        }
+    } while (goal_temp.size() > 0);
+
+    if (answer == goal) {
+        result = "YES";
+    } else {
+        result = "NO";
+    }
+
+    return result;
 }
 
 int main() {
     solution(cards1, cards2, goal);
+    return 0;
 }
