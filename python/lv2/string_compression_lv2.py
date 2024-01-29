31c834ee296051e6c169808ee01786b71db6824c
devggu <pshn123@gmail.com>
2024-01-29 10:44:12 +0900

    asd

---
+++ python/lv2/string_compression_lv2.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = 0
+    return answer
+
+
+#result = 7
+"""
+"aabbaccc"	7
+"ababcdcdababcdcd"	9
+"abcabcdede"	8
+"abcabcabcabcdededededede"	14
+"xababcdcdababcdcd"	17
+"""
+if __name__ == '__main__':
+    s = "aabbaccc"
+    print(solution(s))
