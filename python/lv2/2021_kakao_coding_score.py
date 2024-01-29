0d6107c5ed8fb5a8e27f501806e00e579652b497
devggu <pshn123@gmail.com>
2024-01-29 15:07:03 +0900

    coding_score_bak

--- python/lv2/2021_kakao_coding_score.py
+++ python/lv2/2021_kakao_coding_score.py
@@ -1,32 +1,45 @@
-from ast import parse
 import re
-from turtle import position
 
-info = [
-    "java backend junior pizza 150",
-    "python frontend senior chicken 210",
-    "python frontend senior chicken 150",
-    "cpp backend senior pizza 260",
-    "java backend junior chicken 80",
-    "python backend senior chicken 50",
-]
-
-query = [
-    "java and backend and junior and pizza 100",
-    "python and frontend and senior and chicken 200",
-    "cpp and - and senior and pizza 250",
-    "- and backend and senior and - 150",
-    "- and - and - and chicken 100",
-    "- and - and - and - 150",
-]
 
 class Participant:
     def __init__(self, info):
         self.info = info
         self.parse_info()
-        
+
     def parse_info(self):
-        m = re.match(r'^(java|cpp|python) (frontend|backend) (junior|senior) (pizza|chicken|-) (\d+)$', self.info)
+        m = re.match(
+            r"^(java|cpp|python) (frontend|backend) (junior|senior) (pizza|chicken|-) (\d+)$",
+            self.info,
+        )
+        self.language = m.group(1)
+        self.position = m.group(2)
+        self.career = m.group(3)
+        self.food = m.group(4)
+        self.score = int(m.group(5))
+    
+    def inspect(self, rule):
+        if rule.language != "-" and self.language != rule.language:
+            return False
+        if rule.position != "-" and self.position != rule.position:
+            return False
+        if rule.career != "-" and self.career != rule.career:
+            return False
+        if rule.food != "-" and self.food != rule.food:
+            return False
+        if self.score < rule.score:
+            return False
+        return True
+
+class Rules:
+    def __init__(self, query):
+        self.parse_query(query)
+        self.num_qualified = 0
+
+    def parse_query(self, query):
+        m = re.match(
+            r"^(java|cpp|python|-) and (frontend|backend|-) and (junior|senior|-) and (pizza|chicken|-) (\d+)$",
+            query,
+        )
         self.language = m.group(1)
         self.position = m.group(2)
         self.career = m.group(3)
@@ -34,22 +47,35 @@ class Participant:
         self.score = int(m.group(5))
 
 
-def parse_query(query):
-    m = re.match(r'^(java|cpp|python|-) and (frontend|backend|-) and (junior|senior|-) and (pizza|chicken|-) (\d+)$', query)
-    language = m.group(1)
-    position = m.group(2)
-    career = m.group(3)
-    food = m.group(4)
-    score = int(m.group(5))
-    return language, position, career, food, score
-    
-
-
 def solution(info, query):
     participant_objects = [Participant(i) for i in info]
-    for i in participant_objects:
-        print(i.language, i.position, i.career, i.food, i.score)
-    
-    
-if __name__ == '__main__':
+    rule_objects = [Rules(i) for i in query]
+    answer = []
+    for rule in rule_objects:
+        for participant in participant_objects:
+            if participant.inspect(rule):
+                rule.num_qualified += 1
+        answer.append(rule.num_qualified)
+    print(answer)
+    return answer
+
+
+if __name__ == "__main__":
+    info = [
+        "java backend junior pizza 150",
+        "python frontend senior chicken 210",
+        "python frontend senior chicken 150",
+        "cpp backend senior pizza 260",
+        "java backend junior chicken 80",
+        "python backend senior chicken 50",
+    ]
+
+    query = [
+        "java and backend and junior and pizza 100",
+        "python and frontend and senior and chicken 200",
+        "cpp and - and senior and pizza 250",
+        "- and backend and senior and - 150",
+        "- and - and - and chicken 100",
+        "- and - and - and - 150",
+    ]
     solution(info, query)
--- python/lv2/string_compression_lv2.py
+++ python/lv2/string_compression_lv2.py
@@ -1,9 +1,34 @@
 def solution(s):
     answer = 0
+    candidate = []
+    if len(s) == 1:
+        return 1
+    for i in range(1, len(s) // 2 + 1):
+        temp = ""
+        temp_back = ""
+        cnt = 1
+        candidate_item = ""
+        for j in range(0, len(s)+i, i):
+            temp = s[j : j + i]
+            if temp_back == "":
+                temp_back = temp
+                continue
+            if temp == temp_back:
+                cnt += 1
+            else:
+                if cnt > 1:
+                    candidate_item += str(cnt)
+                candidate_item += temp_back
+                cnt = 1
+            temp_back = temp
+
+        candidate.append(len(candidate_item))
+    print(candidate)
+    answer = min(candidate)
     return answer
 
 
-#result = 7
+# result = 7
 """
 "aabbaccc"	7
 "ababcdcdababcdcd"	9
@@ -11,6 +36,8 @@ def solution(s):
 "abcabcabcabcdededededede"	14
 "xababcdcdababcdcd"	17
 """
-if __name__ == '__main__':
-    s = "aabbaccc"
+
+if __name__ == "__main__":
+    s = "x"
+    print(s[100:200])
     print(solution(s))
