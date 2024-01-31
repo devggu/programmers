ff518d5099d95a1acc4a5324a4c03d2cace8c5dc
devggu <pshn123@gmail.com>
2024-01-31 22:44:04 +0900

    asdasd

--- python/lv2/menu_renewal.py
+++ python/lv2/menu_renewal.py
@@ -7,12 +7,12 @@ import itertools
 #     menu_list = list(set("".join(orders)))
 #     menu_list.sort()
 #     menu_combinations_counter = {}
-    
+
 #     for i in course:
 #         menu_combinations_counter[f"{i}"] = {}
 #         for combination in itertools.combinations(menu_list, i ):
 #             menu_combinations_counter[f"{i}"]["".join(combination)] = 0
-    
+
 #     for course_num in menu_combinations_counter.keys():
 #         for combination in menu_combinations_counter[course_num].keys():
 #             for order in orders:
@@ -20,33 +20,68 @@ import itertools
 #                     menu_combinations_counter[course_num][combination] += 1
 #         max_value = max(menu_combinations_counter[course_num].values())
 #         answer += [k for k,v in menu_combinations_counter[course_num].items() if v == max_value and v > 1]
-    # answer.sort(key= lambda x: x)
-    # return answer
+# answer.sort(key= lambda x: x)
+# return answer
+
+# @timer
+# def solution(orders, course):
+#     answer = []
+#     combinations = {}
+#     done = set()
+#     for order in orders:
+#         i=orders.index(order)
+#         while i < len(orders)-1:
+#             temp = set(order) & set(orders[i+1])
+#             temp = "".join(sorted(temp))
+#             if temp in done:
+#                 i+=1
+#                 continue
+#             if temp != "" and len(temp) > 1:
+#                 if temp not in combinations:
+#                     combinations[temp] = 1
+#                 combinations[temp] += 1
+#             i+=1
+#             if i == len(orders)-1:
+#                 done = done.union(set(combinations.keys()))
+#                 break
+
+#     dict_combinations = {}
+#     for i in course:
+#         dict_combinations[f"{i}"] = {}
+#         for combination in combinations.keys():
+#             if len(combination) == i:
+#                 dict_combinations[f"{i}"][combination] = combinations[combination]
+
+#     for k,v in dict_combinations.items():
+#         if v == {}:
+#             continue
+#         max_value = max(v.values())
+#         answer += [k for k,v in v.items() if v == max_value]
+
+#     answer.sort(key= lambda x: x)
+#     print(done)
+#     print(combinations)
+#     print(dict_combinations)
+#     print(answer)
+
+#     return answer
+
+
+def get_combinations(orders, n):
+    combinations = {}
+    for order in orders:
+        for combination in itertools.combinations(order, n):
+            combination = "".join(sorted(combination))
+            if combination not in combinations:
+                combinations[combination] = 1
+            combinations[combination] += 1
 
 @timer
 def solution(orders, course):
     answer = []
-    combinations = {}
-    done = []
+    
     for order in orders:
-        i=orders.index(order)
-        while i < len(orders)-1:
-            temp = set(order) & set(orders[i+1])
-            temp = "".join(sorted(temp))
-            if temp in done:
-                i+=1
-                continue
-            if temp != "" and len(temp) > 1:
-                if temp not in combinations:
-                    combinations[temp] = 1
-                combinations[temp] += 1
-            i+=1
-            if i == len(orders)-1:
-                done.append(order)
-                break
-    print(done)
-    print(combinations)
-            
+        order
     return answer
 
 
@@ -74,6 +109,6 @@ if __name__ == "__main__":
         course = case[1]
         result = case[2]
         if solution(orders, course) == result:
-            print(f"Test Case{test_cases.index(case)}: Pass")
+            print(f"Test Case{test_cases.index(case)}: Pass\n")
         else:
-            print(f"Test Case{test_cases.index(case)}: Fail")
+            print(f"Test Case{test_cases.index(case)}: Fail\n")
