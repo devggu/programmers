4c8faf89a418caadc249955cde5621d632a02bb3
devggu <pshn123@gmail.com>
2024-01-31 09:00:03 +0900

    asd

--- python_note.py
+++ python_note.py
@@ -1,8 +1,21 @@
-import re
+import OpenOPC
+import time
 
-li = ['#asdasd','@1241as','gaasgasv']
+import pywintypes
 
-for i in li:
-    m = re.match(r'^[^#@].*$', i)
-    if m:
-        print(i)
+pywintypes.datetime = pywintypes.TimeType
+
+opc=OpenOPC.client()
+
+opc.servers()
+
+opc.connect('Matrikon.OPC.Simulation.1')
+tags =['Random.Int1','Random.Real4','Random.Int2','Random.Real8']
+while True:
+   try:
+       value = opc.read(tags,group='Group0',update=1)
+       print (value)
+   except OpenOPC.TimeoutError:
+       print ("TimeoutError occured")
+
+   time.sleep(1)
